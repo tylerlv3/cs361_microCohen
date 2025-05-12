58a65ee553e2b1e9f95a174f58cad0f1e738b368
tyler <tylerlv3@gmail.com>
2025-05-12 23:01:29 +0000

    done for now, functional to post (new+update), and get to rertieve

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy() # Initialize SQLAlchemy without an app instance
 
-# Define a simple model
-class Item(db.Model):
+class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+
+    cMetadata = db.Column(db.JSON, nullable=False)
 
     def __repr__(self):
-        return f"Item('{self.name}')"
+        return f"File('{self.id}')"
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app) 
 
-if __name__ == '__main__':
+    @app.route('/metadata/<int:fileID>', methods=['POST', 'GET'])
+    def metadataFunc(fileID):
+        #post
+        if request.method == 'POST':
+            data= request.get_json()
+            if not data or 'metadata' not in data: 
+                return jsonify({"error": "Data or metadata Missing"}), 400
+            file = File.query.get(fileID)
+            if file:
+                file.cMetadata = data['metadata']
+                db.session.commit()
+                return jsonify({"message": "Metadata updated successfully", "id": fileID}), 200
+            else:
+                new_file = File(id=fileID, cMetadata=data['metadata'])
+                db.session.add(new_file)
+                db.session.commit()
+                return jsonify({"message": "Metadata added successfully", "id": fileID}), 201
+        #Get
+        elif request.method == 'GET':
+            file = File.query.get(fileID)
+            if file:
+                return jsonify(file.cMetadata)
+            else:
+                return jsonify({"error": "File not found"}), 404
+        else:
+            return jsonify({"error": "Method not allowed"}), 405
+
     with app.app_context():
         db.create_all() # Create tables if they don't exist
-    app.run(debug=True)
+
+    return app
+
+if __name__ == '__main__':
+    app_instance = create_app() 
+    app_instance.run(debug=True, port=5004)
---
+++ wsgi.py
@@ -0,0 +1,13 @@
+from app import create_app
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    # Run in debug mode for development with auto-reload
+    # Use a different port than the mainProgram or other services
+    app.run(debug=True, host='0.0.0.0', port=5004) 
